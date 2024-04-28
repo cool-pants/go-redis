564dadc786182e0d756e9b272b0678df10aa1cb2
cool-pants <muban.sg@gmail.com>
2024-04-28 17:02:38 +0530

    feat: finish task 2

--- app/server.go
+++ app/server.go
@@ -1,25 +1,45 @@
 package main
 
 import (
-	"fmt"
-	"net"
-	"os"
+    "fmt"
+    "net"
+    "os"
 )
 
 func main() {
-	// You can use print statements as follows for debugging, they'll be visible when running tests.
-	fmt.Println("Logs from your program will appear here!")
+    // You can use print statements as follows for debugging, they'll be visible when running tests.
+    fmt.Println("Logs from your program will appear here!")
 
-	// Uncomment this block to pass the first stage
-	//
-	l, err := net.Listen("tcp", "0.0.0.0:6379")
-	if err != nil {
-		fmt.Println("Failed to bind to port 6379")
-		os.Exit(1)
-	}
-	_, err = l.Accept()
-	if err != nil {
-		fmt.Println("Error accepting connection: ", err.Error())
-		os.Exit(1)
-	}
+    // Uncomment this block to pass the first stage
+    //
+    l, err := net.Listen("tcp", "0.0.0.0:6379")
+    if err != nil {
+        fmt.Println("Failed to bind to port 6379")
+        os.Exit(1)
+    }
+    c, err := l.Accept()
+    if err != nil {
+        fmt.Println("Error accepting connection: ", err.Error())
+        os.Exit(1)
+    }
+
+    defer c.Close()
+
+    var buf = make([]byte, 128)
+
+    _, err = c.Read(buf)
+    if err != nil {
+        fmt.Println("Failed to read into buffer")
+        os.Exit(1)
+    }
+
+    fmt.Printf("Result : %v\n", string(buf))
+
+    _, err = c.Write([]byte("+PONG\r\n"))
+
+    if err != nil {
+        fmt.Println("Failed to write into buffer")
+        os.Exit(1)
+    }
+
 }

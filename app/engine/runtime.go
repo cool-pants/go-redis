ef65e4d82b20f0e68c6ce55290196c4e74c8755c
cool-pants <muban.sg@gmail.com>
2024-05-12 11:37:11 +0530

    feat: make server a runtime

---
+++ app/engine/runtime.go
@@ -0,0 +1,63 @@
+package engine
+
+import (
+	"fmt"
+	"net"
+	"os"
+	"os/signal"
+	"strings"
+)
+
+
+type RedisConf struct {
+    Host string
+    Port string
+}
+
+func StartEngine(conf RedisConf) {
+    l, err := net.Listen("tcp", fmt.Sprintf("%s:%s", conf.Host, conf.Port))
+    if err != nil {
+        fmt.Println("Failed to bind to port 6379")
+        os.Exit(1)
+    }
+    c, err := l.Accept()
+    if err != nil {
+        fmt.Println("Error accepting connection: ", err.Error())
+        os.Exit(1)
+    }
+
+    defer c.Close()
+
+    listener := make(chan os.Signal, 1)
+    signal.Notify(listener, os.Interrupt)
+    go func(){
+        <-listener
+        fmt.Printf("Shutting down redis\n")
+        os.Exit(0)
+    }()
+    for {
+        connectionReader(c)
+        c.Close()
+    }
+
+}
+
+
+func connectionReader(c net.Conn) {
+    var buf = make([]byte, 128)
+    _, err := c.Read(buf)
+    if err != nil {
+        fmt.Println("Failed to read into buffer")
+        os.Exit(1)
+    }
+    for s := range strings.Split(string(buf), "\r\n") {
+        fmt.Printf("Result : %d\n", s) 
+
+        _, err = c.Write([]byte("+PONG\r\n"))
+
+        if err != nil {
+            fmt.Println("Failed to write into buffer")
+            os.Exit(1)
+        }
+    }
+}
--- app/server.go
+++ app/server.go
@@ -2,9 +2,7 @@ package main
 
 import (
     "fmt"
-    "net"
-    "os"
-    "strings"
+    "github.com/codecrafters-io/redis-starter-go/app/engine"
 )
 
 func main() {
@@ -13,34 +11,10 @@ func main() {
 
     // Uncomment this block to pass the first stage
     //
-    l, err := net.Listen("tcp", "0.0.0.0:6379")
-    if err != nil {
-        fmt.Println("Failed to bind to port 6379")
-        os.Exit(1)
-    }
-    c, err := l.Accept()
-    if err != nil {
-        fmt.Println("Error accepting connection: ", err.Error())
-        os.Exit(1)
-    }
-
-    defer c.Close()
-
-    var buf = make([]byte, 128)
-
-    _, err = c.Read(buf)
-    if err != nil {
-        fmt.Println("Failed to read into buffer")
-        os.Exit(1)
-    }
-    for s := range strings.Split(string(buf), "\r\n") {
-        fmt.Printf("Result : %d\n", s) 
-
-        _, err = c.Write([]byte("+PONG\r\n"))
-
-        if err != nil {
-            fmt.Println("Failed to write into buffer")
-            os.Exit(1)
-        }
-    }
+    engine.StartEngine(
+        engine.RedisConf{
+            Host: "0.0.0.0",
+            Port: "6379",
+        },
+    )
 }

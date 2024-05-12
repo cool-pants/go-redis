d0edbe1f4ceb1826b2f0627b8838e7ebca8dda2c
cool-pants <muban.sg@gmail.com>
2024-05-12 22:00:47 +0530

    fix: fix byte writes into wrong channel

--- app/engine/runtime.go
+++ app/engine/runtime.go
@@ -4,7 +4,6 @@ import (
 	"fmt"
 	"net"
 	"os"
-	"os/signal"
 	"strings"
 )
 
@@ -14,41 +13,28 @@ type RedisConf struct {
     Port string
 }
 
-type EventLoop struct {
-    input chan []byte
-    output chan []byte
-    stop chan struct{}
-    conn *net.Conn
-}
-
 func StartEngine(conf RedisConf) {
     l, err := net.Listen("tcp", fmt.Sprintf("%s:%s", conf.Host, conf.Port))
     if err != nil {
         fmt.Println("Failed to bind to port 6379")
         os.Exit(1)
     }
-    c, err := l.Accept()
-    if err != nil {
-        fmt.Println("Error accepting connection: ", err.Error())
-        os.Exit(1)
+
+    for {
+        c, err := l.Accept()
+        if err != nil {
+            fmt.Println("Error accepting connection: ", err.Error())
+            os.Exit(1)
+        }
+
+        defer c.Close()
+        go connectionReader(c)
     }
+}
 
-    defer c.Close()
 
-    listener := make(chan os.Signal, 1)
-    signal.Notify(listener, os.Interrupt)
-    go func(){
-        <-listener
-        fmt.Printf("Shutting down redis\n")
-        os.Exit(0)
-    }()
-    loop := &EventLoop{
-        input: make(chan []byte, 10),
-        output: make(chan []byte, 10),
-        stop: make(chan struct{}),
-    }
+func connectionReader(c net.Conn) {
 
-    go connectionReader(loop)
     for {
         var buf = make([]byte, 1024)
         _, err := c.Read(buf)
@@ -56,26 +42,15 @@ func StartEngine(conf RedisConf) {
             fmt.Println("Failed to read into buffer")
             os.Exit(1)
         }
-        loop.input<-buf
-
-
-        // Write into route
-        out := <-loop.output
-        fmt.Printf("Received bytes %s\n", string(out))
-        _, err = c.Write(out)
-        if err != nil {
-            fmt.Println("Failed to write into buffer")
-            os.Exit(1)
+        for s, val := range strings.Split(string(buf), "\r\n") {
+            fmt.Printf("Result : %d %s\n", s, val) 
+            _, err = c.Write([]byte("+PONG\r\n"))
+            if err != nil {
+                fmt.Println("Failed to write into buffer")
+                os.Exit(1)
+            }
         }
+
     }
 
-}
-
-
-func connectionReader(eventBuf *EventLoop) {
-    var buf = <-eventBuf.input
-    for s, val := range strings.Split(string(buf), "\r\n") {
-        fmt.Printf("Result : %d %s\n", s, val) 
-        eventBuf.output<-[]byte("+PONG\r\n")
-    }
 }
